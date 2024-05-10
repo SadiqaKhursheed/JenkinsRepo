a58ddaac04728721ab40289150d60622eaee4bde
Sadiqa <sadiqakhursheed@gmail.com>
2024-05-10 09:13:15 +0500

    Assignment 3

---
+++ jenkinsWork.py
@@ -0,0 +1,19 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Function to multiply two numbers
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+# Predefined numbers
+num1 = 10
+num2 = 12
+
+# Perform addition and multiplication
+sum_result = add_numbers(num1, num2)
+product_result = multiply_numbers(num1, num2)
+
+# Display the results
+print("Sum:", sum_result)
+print("Product:", product_result)
